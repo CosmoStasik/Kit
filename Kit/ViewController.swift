dcaee52b87fb095b1947563846aa3d51b81a4219
Stanislav Sobolevski <minuel.colt@gmail.com>
2022-09-04 20:18:05 +0300

    Initial Commit

---
+++ Kit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kit
+//
+//  Created by Stanislav Sobolevsky on 4.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
